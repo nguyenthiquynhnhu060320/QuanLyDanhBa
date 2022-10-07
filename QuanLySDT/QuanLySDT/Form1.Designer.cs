0178e3431727617b3ca1e57390530ccab103a983
nguyenthiquynhnhu060320 <70968782+nguyenthiquynhnhu060320@users.noreply.github.com>
2022-10-08 02:13:07 +0700

    Create project Manager

---
+++ QuanLySDT/QuanLySDT/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿
+namespace QuanLySDT
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.heToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.trợGiúpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.thôngTinPhầnMềmToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txttencoquan = new System.Windows.Forms.TextBox();
+            this.txtnguoidaidien = new System.Windows.Forms.TextBox();
+            this.txtsdt = new System.Windows.Forms.TextBox();
+            this.txtghichu = new System.Windows.Forms.TextBox();
+            this.listvDanhSach = new System.Windows.Forms.ListView();
+            this.menuStrip1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.heToolStripMenuItem,
+            this.trợGiúpToolStripMenuItem,
+            this.thôngTinPhầnMềmToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(857, 30);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // heToolStripMenuItem
+            // 
+            this.heToolStripMenuItem.Name = "heToolStripMenuItem";
+            this.heToolStripMenuItem.Size = new System.Drawing.Size(99, 26);
+            this.heToolStripMenuItem.Text = "Hệ Thống";
+            // 
+            // trợGiúpToolStripMenuItem
+            // 
+            this.trợGiúpToolStripMenuItem.Name = "trợGiúpToolStripMenuItem";
+            this.trợGiúpToolStripMenuItem.Size = new System.Drawing.Size(98, 26);
+            this.trợGiúpToolStripMenuItem.Text = "Trợ Giúp ";
+            // 
+            // thôngTinPhầnMềmToolStripMenuItem
+            // 
+            this.thôngTinPhầnMềmToolStripMenuItem.Name = "thôngTinPhầnMềmToolStripMenuItem";
+            this.thôngTinPhầnMềmToolStripMenuItem.Size = new System.Drawing.Size(187, 26);
+            this.thôngTinPhầnMềmToolStripMenuItem.Text = "Thông Tin Phần Mềm";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label4, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.txttencoquan, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.txtnguoidaidien, 3, 0);
+            this.tableLayoutPanel1.Controls.Add(this.txtsdt, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.txtghichu, 3, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 30);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(3, 10, 3, 3);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.Padding = new System.Windows.Forms.Padding(0, 0, 20, 0);
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(857, 104);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(203, 52);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tên Cơ Quan";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(421, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(203, 52);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Người Đại Diện";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(3, 52);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(203, 52);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Số Điện Thoại";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(421, 52);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(203, 52);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Ghi Chú";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txttencoquan
+            // 
+            this.txttencoquan.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txttencoquan.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txttencoquan.Location = new System.Drawing.Point(212, 10);
+            this.txttencoquan.Margin = new System.Windows.Forms.Padding(3, 10, 3, 3);
+            this.txttencoquan.Name = "txttencoquan";
+            this.txttencoquan.Size = new System.Drawing.Size(203, 30);
+            this.txttencoquan.TabIndex = 4;
+            // 
+            // txtnguoidaidien
+            // 
+            this.txtnguoidaidien.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtnguoidaidien.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtnguoidaidien.Location = new System.Drawing.Point(630, 10);
+            this.txtnguoidaidien.Margin = new System.Windows.Forms.Padding(3, 10, 3, 3);
+            this.txtnguoidaidien.Name = "txtnguoidaidien";
+            this.txtnguoidaidien.Size = new System.Drawing.Size(204, 30);
+            this.txtnguoidaidien.TabIndex = 5;
+            // 
+            // txtsdt
+            // 
+            this.txtsdt.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtsdt.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtsdt.Location = new System.Drawing.Point(212, 62);
+            this.txtsdt.Margin = new System.Windows.Forms.Padding(3, 10, 3, 3);
+            this.txtsdt.Name = "txtsdt";
+            this.txtsdt.Size = new System.Drawing.Size(203, 30);
+            this.txtsdt.TabIndex = 6;
+            // 
+            // txtghichu
+            // 
+            this.txtghichu.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtghichu.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtghichu.Location = new System.Drawing.Point(630, 62);
+            this.txtghichu.Margin = new System.Windows.Forms.Padding(3, 10, 3, 3);
+            this.txtghichu.Name = "txtghichu";
+            this.txtghichu.Size = new System.Drawing.Size(204, 30);
+            this.txtghichu.TabIndex = 7;
+            // 
+            // listvDanhSach
+            // 
+            this.listvDanhSach.BackColor = System.Drawing.Color.PapayaWhip;
+            this.listvDanhSach.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listvDanhSach.HideSelection = false;
+            this.listvDanhSach.Location = new System.Drawing.Point(0, 134);
+            this.listvDanhSach.Name = "listvDanhSach";
+            this.listvDanhSach.Size = new System.Drawing.Size(857, 392);
+            this.listvDanhSach.TabIndex = 2;
+            this.listvDanhSach.UseCompatibleStateImageBehavior = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(857, 526);
+            this.Controls.Add(this.listvDanhSach);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem heToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem trợGiúpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem thôngTinPhầnMềmToolStripMenuItem;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txttencoquan;
+        private System.Windows.Forms.TextBox txtnguoidaidien;
+        private System.Windows.Forms.TextBox txtsdt;
+        private System.Windows.Forms.TextBox txtghichu;
+        private System.Windows.Forms.ListView listvDanhSach;
+    }
+}
+
